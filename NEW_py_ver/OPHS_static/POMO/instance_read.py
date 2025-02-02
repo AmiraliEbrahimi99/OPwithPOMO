de086438c6fa2e18da1cf2995ee1290a2c6c148b
Amirali <amiraebi772@gmail.com>
2025-02-02 16:09:27 +0330

    main-22: "Fix": stochastic instance generate and .pt creation

--- NEW_py_ver/OPHS_static/POMO/instance_read.py
+++ NEW_py_ver/OPHS_static/POMO/instance_read.py
@@ -132,37 +132,37 @@ def process_ophssp_file(filepath, output_dir):
     # Rescale node_prize_tensor to have values between 0.02 and 0.99
     mean_min = mean_tensor.min()
     mean_max = mean_tensor.max()
-    mean_rescaled = (mean_tensor - mean_min) / (mean_max - mean_min) * (0.99 - 0.02) + 0.02
+    mean_rescaled = (mean_tensor - mean_min) / (mean_max - mean_min) * (0.955 - 0.045) + 0.045
 
     deviation_min = deviation_tensor.min()
     deviation_max = deviation_tensor.max()
     deviation_rescaled = (deviation_tensor - deviation_min) / (deviation_max - deviation_min) * (0.16 - 0.015) + 0.015
 
-    # Rescale remain_length based on the same scaling factors used for node coordinates
     remaining_length_rescaled = remaining_length_tensor / torch.tensor([x_max - x_min, y_max - y_min]).max()
 
     # print("\nRemaining lenth rescaled:", remaining_length_rescaled)
     # print("Hotel xy rescaled tensor:", hotel_xy_rescaled)
     # print("Node xy rescaled tensor:", node_xy_rescaled)
-    # print("Prize rescaled tensor:", prize_rescaled)
-    # print(f"\n\n deviation rescaled: {deviation_rescaled}")
+    # print("mean rescaled tensor:", mean_rescaled)
+    # print("deviation rescaled tensor:",deviation_rescaled)
 
     remaining_len = remaining_length_rescaled.unsqueeze(0)
     hotel_xy = hotel_xy_rescaled.unsqueeze(0)
     node_xy = node_xy_rescaled.unsqueeze(0)
-    # node_prize = prize_rescaled.unsqueeze(0)
+    mean = mean_rescaled.unsqueeze(0)
+    deviation = deviation_rescaled.unsqueeze(0)
 
-    # Create a dictionary to store the tensors
     saved_dict = {
         'remain_len': remaining_len,
         'hotel_xy': hotel_xy,
         'node_xy': node_xy,
-        # 'node_prize': node_prize 
+        'mean': mean, 
+        'deviation': deviation 
     }
 
     # Save the dictionary to a file
-    # output_filename = os.path.join(output_dir, os.path.basename(filepath).replace('.ophs', '.pt'))
-    # torch.save(saved_dict, output_filename)
+    output_filename = os.path.join(output_dir, os.path.basename(filepath).replace('.ophs', '.pt'))
+    torch.save(saved_dict, output_filename)
 
 
 def ophssp_create_new_file(filepath, output_dir):
@@ -202,14 +202,15 @@ def ophssp_create_new_file(filepath, output_dir):
     # Rescale node_prize_tensor to have values between 0.02 and 0.99
     prize_min = node_prize_tensor.min()
     prize_max = node_prize_tensor.max()
-    prize_rescaled = (node_prize_tensor - prize_min) / (prize_max - prize_min) * (0.99 - 0.02) + 0.02
+    prize_rescaled = ((node_prize_tensor - prize_min) / (prize_max - prize_min) * (95.5 - 4.5) + 4.5)
 
-    deviation_generator = (torch.rand(problem_size) * (16 - 1.5) + 1.5) / 100  
-    rescaled_deviation = deviation_generator * (node_prize_tensor / prize_rescaled)
-    rescaled_variance = rescaled_deviation ** 2
-    print(f'\n{node_prize_tensor}\n{prize_rescaled}\n{deviation_generator}\n {rescaled_deviation}\n \n')
+    coefficient_of_variation = (torch.rand(problem_size)) * (0.5 - 0.1) + 0.1
+    deviation = prize_rescaled * coefficient_of_variation 
+    rescaled_deviation = ((deviation - 4.5) * (prize_max - prize_min) / (95.5 - 4.5)) + prize_min
+    rescaled_variance = (rescaled_deviation) ** 2
 
-    # Initialize the modified lines with the first line
+    # print(f'\n{node_prize_tensor}\n{rescaled_deviation}\n{coefficient_of_variation} \n')
+
     modified_lines = [first_line]  # Include the first line at the beginning
     modified_lines.extend(lines[:3 + hotel_size])  # Keep the first 3+hotel_size lines unchanged
 
@@ -219,9 +220,7 @@ def ophssp_create_new_file(filepath, output_dir):
         y = float(nums[1]) if "." in nums[1] else int(nums[1])
         z = int(nums[2])  # Always an integer
 
-        # Format while maintaining alignment
-        formatted_line = f"{x:<7} {y:<7} {z:<4d} {rescaled_variance[i].item():<7.3f}\n"
-        # formatted_line = f"{float(nums[0]):<7.2f} {float(nums[1]):<7.2f} {int(nums[2]):<4d} {rescaled_variance[i].item():<7.3f}\n"
+        formatted_line = f"{x:<7} {y:<7} {z:<4d} {rescaled_variance[i].item():<7.2f}\n"
         modified_lines.append(formatted_line)
 
     modified_lines.append('-------------------------------------------------------------------\n')
@@ -232,16 +231,15 @@ def ophssp_create_new_file(filepath, output_dir):
 
 ###########################################################################################################################
 
-
-# Directory containing the folders and files
-root_dir = "../../test_instances/OPHS_instances_February_2013/SET1 1-2/64-45-1-2.ophs"
-output_dir = "../Instances"
+root_dir = "Instances/raw_OPHSSP_instances_high_variance"
+output_dir = "Instances/OPHSSP_high_variance_pt"
 # output_dir = "Instances/OPHSSP"
 os.makedirs(output_dir, exist_ok=True)
 
-# Iterate over all .ophs files in the directory and subdirectories
-# for filepath in glob.glob(os.path.join(root_dir, "**/*.ophs"), recursive=True):
-#     ophssp_create_new_file(filepath, output_dir)
+for filepath in glob.glob(os.path.join(root_dir, "**/*.ophs"), recursive=True):
+    process_ophssp_file(filepath, output_dir)
 
-ophssp_create_new_file(root_dir, output_dir)
-# process_ophssp_file(root_dir2, output_dir)
+################ test #############################
+
+# ophssp_create_new_file(root_dir, output_dir)
+# process_ophssp_file(root_dir, output_dir)
